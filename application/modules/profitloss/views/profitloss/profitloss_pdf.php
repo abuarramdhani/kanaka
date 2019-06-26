5c9ef699992143d7bbfdd0a7312a111a694a13ee
octa <octa@nadyne.com>
2019-06-26 10:21:57 +0700

    Add Print PDF Excel Profit and Loss

--- application/modules/profitloss/views/profitloss/profitloss_pdf.php
+++ application/modules/profitloss/views/profitloss/profitloss_pdf.php
@@ -2,66 +2,285 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Untitled Document</title>
+	<title>Untitled Document</title>
+	
+	<style>
+		body,table th,td{
+			font-size: 11px;
+			font-family: sans-serif;
+		}
+		
+		@page { margin: 10px 10px; }
+		#header { left: 0px; top: 0px; right: 0px; text-align: center;  }
+		#footer { left: 0px; bottom: 30px; right: 0px; font-size: 11px; font-family: sans-serif; text-align:right; }	
+		#content{
+			border-bottom:0px solid #000000;
+		}
+		#footer .page:after { content: counter(page, upper-roman); }
+		#content{
+		background-color:#FFFFFF;
+		} 
+	</style>
+
 </head>
 
 <body>
+	<div id="header">
+		<img src="<?=base_url()?>assets/img/logo.png" alt="Logo Kanaka" width="200" />
+		<h2 style="text-align: left;">PT. KANAKA GRAHA PARAMITHA</h2>
+		<h4 style="text-align: left;"><?php echo lang('profit_loss'); ?></h4>
+		<h4 style="text-align: left;"><?php echo lang('period') . ' ' . $period ?></h4>
+	</div>
 
-<style>
-    body,table th,td{
-        font-size: 11px;
-        font-family: sans-serif;
-    }
-	
-	 @page { margin: 10px 10px; }
-     #header { left: 0px; top: 0px; right: 0px; text-align: center;  }
-     #footer { left: 0px; bottom: 30px; right: 0px; font-size: 11px; font-family: sans-serif; text-align:right; }	
-	  #content{
-    	 border-bottom:0px solid #000000;
-	 }
-     #footer .page:after { content: counter(page, upper-roman); }
-	#content{
-	background-color:#FFFFFF;
-	} 
-</style>
+	<div id="content" style="text-align:center;">
+		<table width="100%" border="1" cellpadding="1" cellspacing="0">
+			<tr>
+				<th>I</th>
+				<th align="left">Penjualan</th>
+				<td colspan="3">&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Penjualan</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($penjualan, 0) ?></td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Retur Penjualan</td>
+				<td class="text-right"><?php echo number_format($retur_penjualan, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Penjualan Bersih</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($penjualan_bersih, 0) ?></td>
+			</tr>
 
-<div id="header">
-    <h2><?php echo lang('zona'); ?></h2>
-</div>
+			<tr>
+				<td colspan="5">&nbsp;</td>
+			</tr>
 
-<div id="content" style="text-align:center;">
-<table width="100%" border="1" cellpadding="1" cellspacing="0">
-	<tr >
-		<th  align="center" width="5%" height="20px">No</th>
-		<th  align="center"><?=lang('name')?></th>
-		<th  align="center"><?=lang('description')?></th>
-		<th  align="center"><?=lang('created_date')?></th>
-	</tr>
-	<?php 
-    $i=0;
-    if(count($zonas) > 0){
-	   foreach($zonas as $zona){
-	   $i++;
-   	?>
-					
-	<tr style="font-size:9px">
-		<td align="center"><?php echo $i;?></td>
-		<td><?=$zona->name?></td>
-		<td><?=$zona->description?></td>
-		<td><?=date('d-m-Y',strtotime($zona->date_created))?></td>
-	</tr>
-	<?php 
-        }
-    }
-    else{
-    ?>
-        <tr style="font-size:9px">
-		  <td align="center" colspan="4">No Data Available</td>
-        </tr>
-    <?php
-    }
-    ?>
-</table>
-</div>
+			<tr>
+				<th>II</th>
+				<th align="left">HPP</th>
+				<td colspan="3">&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Persediaan Barang Dagang Awal</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($persediaan_barang_dagang_awal, 0) ?></td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Pembelian</td>
+				<td class="text-right"><?php echo number_format($pembelian, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Beban angkut Pembelian</td>
+				<td class="text-right"><?php echo number_format($beban_angkut_pembelian, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Potongan Pembelian</td>
+				<td class="text-right"><?php echo number_format($potongan_pembelian, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Retur Pembelian</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($retur_pembelian, 0) ?></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Pembelian Bersih</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($pembelian_bersih, 0) ?></td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Barang Siap Jual</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($barang_siap_jual, 0) ?></td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Persediaan Akhir</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($persediaan_akhir, 0) ?></td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>HPP</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($hpp, 0) ?></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Laba Kotor</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($laba_kotor, 0) ?></td>
+			</tr>
+
+			<tr>
+				<td colspan="5">&nbsp;</td>
+			</tr>
+
+			<tr>
+				<th>III</th>
+				<th align="left">Beban Operational</th>
+				<td colspan="3">&nbsp;</td>
+			</tr>
+			<tr>
+				<th>&nbsp;</th>
+				<th align="left">Beban Penjualan</th>
+				<td colspan="3">&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Beban Angkut Penjualan</td>
+				<td class="text-right"><?php echo number_format($beban_angkut_penjualan, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Beban Asuransi Toko</td>
+				<td class="text-right"><?php echo number_format($beban_asuransi_toko, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Beban Iklan</td>
+				<td class="text-right"><?php echo number_format($beban_iklan, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Baban Perlengkapan Toko</td>
+				<td class="text-right"><?php echo number_format($beban_perlengkapan_toko, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Total Beban Penjualan</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($total_beban_penjualan, 0) ?></td>
+				<td>&nbsp;</td>
+			</tr>
+
+			<tr>
+				<td colspan="5">&nbsp;</td>
+			</tr>
+
+			<tr>
+				<th>&nbsp;</th>
+				<th align="left">Beban Penjualan dan Administrasi</th>
+				<td colspan="3">&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Beban Gaji</td>
+				<td class="text-right"><?php echo number_format($beban_gaji, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Beban Utilitas</td>
+				<td class="text-right"><?php echo number_format($beban_utilitas, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Beban Perlengkapan Kantor</td>
+				<td class="text-right"><?php echo number_format($beban_perlengkapan_kantor, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Beban Sewa</td>
+				<td class="text-right"><?php echo number_format($beban_sewa, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Beban Penyusutan</td>
+				<td class="text-right"><?php echo number_format($beban_penyusutan, 0) ?></td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Total Beban Umum dan Administrasi</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($total_beban_umum_dan_administrasi, 0) ?></td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Total Beban Operational</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($total_beban_operasional, 0) ?></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Rugi Operational</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($rugi_operasional, 0) ?></td>
+			</tr>
+
+			<tr>
+				<td colspan="5">&nbsp;</td>
+			</tr>
+
+			<tr>
+				<th>IV</th>
+				<th align="left">Pendapatan dan Beban Luar Usaha</th>
+				<td colspan="3">&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Pendapatan Bunga</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($pendapatan_bunga, 0) ?></td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>Laba Bersih</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td class="text-right"><?php echo number_format($laba_bersih, 0) ?></td>
+			</tr>
+		</table>
+	</div>
 </body>
 </html>
